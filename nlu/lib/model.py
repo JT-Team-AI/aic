fc4b303007a76d7708b4fc7610d7dbfdc233d766
Naveed Afzal <naveed_999@yahoo.com>
2018-03-24 14:26:57 +0900

    add IntentClassifier

--- nlu/lib/model.py
+++ nlu/lib/model.py
@@ -3,6 +3,8 @@ from logging import getLogger
 from sklearn import metrics
 from sklearn import neural_network
 
+from lib.utils import read_json
+
 import json
 import numpy as np
 import pandas as pd
@@ -12,7 +14,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.preprocessing import LabelBinarizer
 
 logger = getLogger(__name__)
 
@@ -27,22 +29,21 @@ class IntentClassifier(object):
     def load_data(self, path="dataset.json"):
 
         # Get data
-        with open(path, 'r', encoding='utf-8') as f:
-            data = json.load(f)
-
-        self.documents = [i['title'] + "\n" + i['description'] for i in data]
-        self.labels = [i['tags'] for i in data]
+        train_data = read_json(path)
+        examples = train_data['rasa_nlu_data']['common_examples']
+        self.texts = [x['text'] for x in examples]
+        self.intents = [x['intent'] for x in examples]
 
         # Set label binarizer
-        self.label_binarizer = MultiLabelBinarizer()
-        self.label_binarizer.fit(self.labels)
+        self.label_binarizer = LabelBinarizer()
+        self.label_binarizer.fit(self.intents)
         self.classes = self.label_binarizer.classes_
 
     def train(self, max_iter=200):
 
         self.count_vectorizer = CountVectorizer(tokenizer=self.tokenizer)
-        self.count_vectorizer.fit(self.documents)
-        counts = self.count_vectorizer.transform(self.documents)
+        self.count_vectorizer.fit(self.texts)
+        counts = self.count_vectorizer.transform(self.texts)
         self.vocabulary = self.count_vectorizer.vocabulary_
 
         self.tfidf_transformer = TfidfTransformer()
@@ -51,7 +52,7 @@ class IntentClassifier(object):
         logger.info("Training Supervised model")
 
         self.X = self.tfidf_transformer.transform(counts)
-        self.y = self.label_binarizer.transform(self.labels)
+        self.y = self.label_binarizer.transform(self.intents)
 
         self.model = neural_network.MLPClassifier(max_iter=max_iter)
         self.model.fit(self.X, self.y)
@@ -72,22 +73,7 @@ class IntentClassifier(object):
         self.count_vectorizer = CountVectorizer(tokenizer=self.tokenizer,
                                                 vocabulary=self.vocabulary)
 
-    def save_classification_report(self, report, path="./reports/report.csv"):
-        report_data = []
-        lines = report.split('\n')
-        for line in lines[2:-3]:
-            row = {}
-            row_data = line.lstrip().split('      ')
-            row['class'] = row_data[0]
-            row['precision'] = float(row_data[1])
-            row['recall'] = float(row_data[2])
-            row['f1_score'] = float(row_data[3])
-            row['support'] = float(row_data[4])
-            report_data.append(row)
-        dataframe = pd.DataFrame.from_dict(report_data)
-        dataframe.to_csv(path, index=False)
-
-    def evaluate(self, test_size=0.2, max_iter=200, report_path="./reports/report.csv", threshold=0.5):
+    def evaluate(self, test_size=0.2, max_iter=200, threshold=0.5):
 
         self.test_vectorizer = TfidfVectorizer(tokenizer=self.tokenizer)
         self.test_model = neural_network.MLPClassifier(max_iter=max_iter)
@@ -95,8 +81,8 @@ class IntentClassifier(object):
         logger.info("Generating train test split with %f/%f ratio", 1-test_size, test_size)
 
         # Train test split
-        self.doc_train, self.doc_test, self.label_train, self.label_test = train_test_split(self.documents,
-                                                                                            self.labels,
+        self.doc_train, self.doc_test, self.label_train, self.label_test = train_test_split(self.texts,
+                                                                                            self.intents,
                                                                                             test_size=test_size,
                                                                                             random_state=42)
         # Vectorize X
@@ -112,13 +98,8 @@ class IntentClassifier(object):
         self.test_model.fit(self.X_train, self.y_train)
 
         logger.info("Testing supervised model")
-        self.probs = self.test_model.predict_proba(self.X_test)
-        self.predicted = (self.probs > threshold).astype(int)
-#       self.predicted = self.test_model.predict(self.X_test)
-
-        self.report = metrics.classification_report(self.y_test, self.predicted, target_names=self.classes)
-        self.save_classification_report(self.report, path=report_path)
-        logger.info("Classification Report saved to: %s", report_path)
+        self.predicted = self.test_model.predict(self.X_test)
+#        self.predicted = self.classes[np.argmax(self.probs)]
 
         self.f1_score_micro = metrics.f1_score(self.y_test, self.predicted, average="micro")
         self.f1_score_weighted = metrics.f1_score(self.y_test, self.predicted, average="weighted")
@@ -126,10 +107,14 @@ class IntentClassifier(object):
         logger.info("F-1 Score (micro): %f", self.f1_score_micro)
         logger.info("F-1 Score (weighted): %f", self.f1_score_weighted)
 
-    def infer(self, query, threshold=0.5):
+    def infer(self, query):
         query_counts = self.count_vectorizer.transform(query)
         query_x = self.tfidf_transformer.transform(query_counts)
         prob = self.model.predict_proba(query_x)[0]
-        pred = (prob > threshold).astype(int)
-        res = self.classes[np.where(pred)]
-        return res
+        pred = self.classes[np.argmax(prob)]
+        score = prob[np.argmax(prob)]
+        result =  {
+            'top_intent': pred,
+            'score': score
+        }
+        return result
