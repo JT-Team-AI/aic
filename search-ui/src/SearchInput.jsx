4f5ab224546862d3bcd36162d98479909af588a4
sndyuk <sanada@sndyuk.com>
2018-03-24 23:09:22 +0900

    Add voice recognition

--- search-ui/src/SearchInput.jsx
+++ search-ui/src/SearchInput.jsx
@@ -5,11 +5,35 @@ import { Observable } from 'rxjs/Observable';
 import { intent } from './state/actions';
 
 class SearchInput extends React.Component {
+  startDictation(onChange) {
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+      const recognition = new webkitSpeechRecognition(); // eslint-disable-line no-undef
+  
+      recognition.continuous = false;
+      recognition.interimResults = false;
+  
+      recognition.lang = "en-US";
+      recognition.start();
+  
+      recognition.onresult = (e) => {
+        const result = e.results[0][0].transcript;
+        document.getElementById('sentence').value = result;
+        recognition.stop();
+        onChange(result);
+      };
+  
+      recognition.onerror = (e) => {
+        recognition.stop();
+      }
+    }
+  }
+
   render() {
     const { onChange } = this.props;
-    return (
+    return (<div>
       <input
         className="form-control"
+        id="sentence"
         aria-describedby="sizing-addon1"
         type="search"
         placeholder="Type what you want to do"
@@ -32,7 +56,8 @@ class SearchInput extends React.Component {
           this.textInput = node;
         }}
       />
-    );
+      <div><img onClick={() => this.startDictation(onChange)} src="//i.imgur.com/cHidSVu.gif" /></div>
+    </div>);
   }
 }
 
