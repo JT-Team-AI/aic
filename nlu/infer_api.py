72c3ef53f5f22b7b7d501e99ac92aa72fe236d13
Naveed Afzal <naveed_999@yahoo.com>
2018-03-24 10:53:44 +0900

    nlu initial setup

---
+++ nlu/infer_api.py
@@ -0,0 +1,91 @@
+from lib.app_init import is_production
+
+import sys
+import traceback
+
+# from logging import getLogger
+from flask import Flask
+from flask import jsonify
+from flask import request
+from langdetect import detect
+
+from lib.model import IntentClassifier
+from lib.tokenizer import KuromojiTokenizer
+
+
+"""
+Sample json request data:
+{
+    "title": "content title here",
+    "description": "content description here",
+    "language": "auto",
+    "threshold": "0.5"
+}
+
+Sample request:
+curl -i -H "Content-Type: application/json" -X POST -d '{"title": "kyoto", "description": "osaka", "language": "en", "threshold": "0.5"}' http://localhost:5000/infer
+
+For Windows:
+curl -i -H "Content-Type: application/json" -X POST -d "{\"title\": \"kyoto\", \"description\": \"osaka\", \"language\": \"en\", \"threshold\": \"0.5\"}" http://localhost:5500/infer
+
+"""
+
+app = Flask(__name__)
+
+
+@app.route('/infer', methods=['POST'])
+def predict():
+    try:
+        json_ = request.get_json()
+        query = json_["title"] + "\n" + json_["description"]
+
+        # Automatic language detection
+        if json_["language"] == "auto":
+            language = detect(query)
+            app.logger.debug("Detected Language: %s", language)
+
+            if language != 'en' and language != 'ja':
+                language = 'ja'
+                app.logger.debug("Defaulting to ja")
+
+        # Use specified language
+        else:
+            language = json_["language"]
+
+        if language == "en":
+            predictions = list(
+                en_model.infer([query], float(json_["threshold"]))
+                )
+
+        elif language == "ja":
+            predictions = list(
+                ja_model.infer([query], float(json_["threshold"]))
+                )
+
+        return jsonify({'predictions': predictions})
+
+    except Exception as e:
+        return 'Error' if is_production else jsonify(
+            {'error': str(e), 'trace': traceback.format_exc()}
+            )
+
+
+try:
+    port = int(sys.argv[1])
+except Exception as e:
+    port = 5000
+
+try:
+    ja_model = IntentClassifier(tokenizer=KuromojiTokenizer().tokenize)
+    ja_model.load_model("models/ja_model.pkl")
+    app.logger.info('Model loaded: models/ja_model.pkl')
+    en_model = IntentClassifier(tokenizer=None)
+    en_model.load_model("models/en_model.pkl")
+    app.logger.info('Model loaded: models/en_model.pkl')
+
+
+except Exception as e:
+    app.logger.error(e)
+    sys.exit(0)
+
+# app.run(host='0.0.0.0', port=port, debug=True)
---
+++ nlu/main.py
@@ -0,0 +1,66 @@
+# from lib.utils import tinysegmenter_tokenize
+import lib.app_init  # noqa: F401
+from lib.model import IntentClassifier
+from lib.tokenizer import KuromojiTokenizer
+from lib.utils import preprocess
+from logging import getLogger
+
+import argparse
+import json
+
+logger = getLogger(__name__)
+
+# Command line arguments
+parser = argparse.ArgumentParser()
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument("--train", action="store_true")
+group.add_argument("--evaluate", action="store_true")
+group.add_argument("--infer", action="store_true")
+
+# Model arguments
+parser.add_argument('--model_path', type=str, default="./models/model.pkl", help='Output path for trained model')
+parser.add_argument('--data_path', type=str, default="en_data.json", help='Path to processed dataset for training')
+parser.add_argument('--tokenizer', type=str, default="en", help='Language of training data, en or ja')
+parser.add_argument('--threshold', type=float, default=0.5, help='Confidence threshold for prediction')
+parser.add_argument('--split_ratio', type=float, default=0.8, help='Ratio of training data for evaluation')
+parser.add_argument('--iterations', type=int, default=200, help='Maximum iterations for MLP training')
+parser.add_argument('--query', type=str, default="", help='Text to infer tags from')
+parser.add_argument("--query_type", type=str, default="TEXT", help='Set query format, TEXT or JSON')
+
+args = parser.parse_args()
+
+if args.tokenizer == "ja":
+    tokenizer = KuromojiTokenizer().tokenize
+elif args.tokenizer == "en":
+    tokenizer = None
+
+predictor = IntentClassifier(tokenizer=tokenizer)
+
+
+if args.train:
+    predictor.load_data(path=args.data_path)
+    predictor.train(max_iter=args.iterations)
+    predictor.save_model(path=args.model_path)
+
+elif args.evaluate:
+    predictor.load_data(path=args.data_path)
+    predictor.evaluate(test_size=(1-args.split_ratio),
+                       max_iter=args.iterations,
+                       report_path=args.report_path,
+                       threshold=args.threshold)
+
+elif args.infer:
+    predictor.load_model(args.model_path)
+
+    if args.query_type == "JSON":
+        dictionary = json.loads(args.query)
+        q = dictionary['title'] + "\n" + dictionary['description']
+    else:
+        q = args.query
+
+    predictions = predictor.infer([q], args.threshold)
+    logger.info("Predicted tags are: %s", predictions)
+
+else:
+    logger.warn("No mode specified")
